76a92ed7bcb5dabce5b77da66f9a4cc7317f2069
joycebustillo <joycebustillo@evsu.edu.ph>
2025-09-17 13:44:51 +0800

    Created project config, core app, extend base, home and about  html

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello, Evsu")
+
+def home(request):
+    return render(request, 'home.html', {'title': 'Home'})
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
